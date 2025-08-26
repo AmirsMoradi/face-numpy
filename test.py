824f026c9f63df043f9b65a49cc1696bf2ba0816
Amirhossein <167887406+AmirsMoradi@users.noreply.github.com>
2025-08-26 17:28:06 +0330

    Update test.py

--- test.py
+++ test.py
@@ -3,19 +3,15 @@ import numpy as np
 import joblib
 from PIL import Image
 
-# بارگذاری مدل آموزش‌دیده با joblib
 model = joblib.load("adaboost_face_model.pkl")
 
-# تابع تغییر اندازه تصویر
 def resize_image(img, target_shape=(50, 37)):
     return np.array(Image.fromarray(img).resize(target_shape))
 
-# تابع برای پیش‌بینی چهره در یک ناحیه
 def predict_face_region(model, region):
     resized = resize_image(region)
     return model.predict([resized.flatten()])[0] == 1
 
-# استریم ویدیو
 cap = cv2.VideoCapture(0)
 
 while True:
@@ -23,10 +19,8 @@ while True:
     if not ret:
         break
 
-    # تبدیل به grayscale
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # اسکن تصویر با sliding window
     window_size = (50, 37)
     step_size = 10
     for y in range(0, gray.shape[0] - window_size[1], step_size):
@@ -35,7 +29,6 @@ while True:
             if region.shape[:2] == window_size and predict_face_region(model, region):
                 cv2.rectangle(frame, (x, y), (x + window_size[0], y + window_size[1]), (0, 255, 0), 2)
 
-    # نمایش فریم
     cv2.imshow("Face Detection", frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -43,3 +36,4 @@ while True:
 
 cap.release()
 cv2.destroyAllWindows()
+
